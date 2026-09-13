01d1c070b39da8fa311391968ecea970a7c92c60
Zain <za4innn@gmail.com>
2026-09-13 22:36:14 +0500

    Fighting with pointers

--- pointers.cpp
+++ pointers.cpp
@@ -25,8 +25,23 @@ int main(){
     std::cout << *pPizzas <<'\n';
     std::cout << pPizzas <<'\n';
 
-    
 
+
+    //null pointers
+    
+    int *pointer = nullptr;
+    int x = 123;
+
+    pointer = &x;
+
+    if(pointer == nullptr){
+        std::cout << "address was not assigned!\n";
+    }
+    else{
+        std::cout << "address was assigned!\n";
+        std::cout << *pointer;
+    }
+    
 
     return 0;
 }
