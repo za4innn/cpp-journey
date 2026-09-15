f43c859d311df760cdaa9c7438bff6b24908ddc5
Zain <za4innn@gmail.com>
2026-09-15 22:02:57 +0500

    Getting started with Pointers

--- pointers.cpp
+++ pointers.cpp
@@ -59,5 +59,39 @@ int main(){
     delete ptr;
     ptr = nullptr;
 
+
+     // dynamic memory = Memory that is allocated after the program 
+    //                  is already compiled and running.
+    //                  'Use' the new operator to allocate memory 
+    //                  in the heap rather than the stack
+
+    //                  Useful when we don't know how much memory
+    //                  we will need.Makes our program more flexible 
+    //                  especially when accepting user input.
+
+
+
+    char *pGrades = nullptr;
+    int size;
+
+    std::cout << "Enter number of grades : ";
+    std::cin >> size;
+
+    pGrades = new char[size];
+
+    for(int i = 0; i < size; i++){
+        std::cout << "Enter Grade #" << i + 1 << " : ";
+        std::cin >> pGrades[i];
+
+    }
+
+
+    for(int i = 0; i < size; i++){
+        std::cout << pGrades[i] << " ";
+
+    }
+
+    delete[] pGrades;
+
     return 0;
 }
