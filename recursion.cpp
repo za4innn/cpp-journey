121a8a6704e9d98b1c6e4d6e14309f44b58bb595
Zain <za4innn@gmail.com>
2026-09-16 20:14:22 +0500

    Understanding Recursion

---
+++ recursion.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void eat(int pizza);
+void drink(int soda);
+
+int main(){
+
+    
+// recursion = a programming technique where a function invokes itself from within
+//             break a complex concept into a repeatable single step
+
+// (iterative vs recursive)
+
+//  advantages      =   less code and is cleaner
+//                      useful for sorting and searching algorithms
+//  disadvantages   =   uses more memory
+//                      slower
+
+eat(20);
+drink(10);
+
+    return 0;
+}
+
+void eat(int pizza){
+    for(int i = 0; i < pizza; i++){
+      std::cout << "You eat a pizza!\n";    //iterative 
+    }
+}
+
+void drink(int soda){
+    if(soda > 0){
+        std::cout << "You drink a soda can!\n";   //recursive
+        drink(soda - 1);                   
+    }
+}
