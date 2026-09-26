4ba11cafc8e7d21e28404bb0b59ea3e4b5dc3c1d
Zain <za4innn@gmail.com>
2026-09-26 12:19:49 +0500

    Created a dynamic array using pointers

--- dynamicarray.cpp
+++ dynamicarray.cpp
@@ -71,5 +71,77 @@ int main(){
     delete[] arr;
     arr = nullptr;
 
+
+
+
+    //Created another dynamic array
+    int length = 0;
+    int space;
+
+    std::cout <<"Enter initial space : ";
+    std::cin >> space;
+
+    int* arr2 = new int[space];
+
+    //Add numbers
+    int value1;
+    std::cout << "Enter " << space << " elements : ";
+    for(int i = 0; i < space; i++){
+       std::cin >> arr2[i];
+       length++;
+    }
+
+    //Add another elements
+    std::cout << "Enter another element : ";
+    std::cin >> value1;
+    if(length == space){
+        space *= 2;
+
+        int* temporary = new int[space];
+
+        for(int i = 0; i < length; i++){
+            temporary[i] = arr2[i];
+        }
+
+        delete[] arr2;
+        arr2 = temp;
+
+    }
+
+    arr2[length] = value1;
+    length++;
+
+    //Display result
+    std::cout << "After adding : ";
+    for(int i = 0; i < length; i++){
+        std::cout << arr2[i] << " ";
+    }
+
+    //Removing through index
+    int indexx;
+
+    std::cout << "\nEnter an index to remove an element : ";
+    std::cin >> indexx;
+
+    if(index >= 0 && indexx < length ){
+       for(int i = indexx; i < length - 1; i++){
+          arr2[i] = arr2[i + 1];
+       }
+       length--;
+    }
+    else{
+        std::cout <<"Invalid index";
+    }
+
+    //Display result after removal
+    std::cout << "After removal : ";
+    for(int i = 0; i < length; i++){
+        std::cout << arr[i] << " ";
+    }
+
+    //Free memory
+    delete[] arr2;
+    arr2 = nullptr;
+
     return 0;
 }
